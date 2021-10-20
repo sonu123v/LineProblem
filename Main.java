a458bb5c1f41e12a2516f97aace245b1bdfbe5cc
sonu parihar <sparihar729@gmail.com>
2021-10-20 18:14:24 +0530

    main runner is added

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz.LinecomparisionProblem;
+public class Main {
+    public static void main ( String[] args ) {
+        LineComparison lineComparison = new LineComparison ();
+        System.out.println ( "*********************************************\n    Welcome To Line Comparison Computation \n*********************************************" );
+        System.out.println ( "\nEnter line 1 coordinates" );
+        int line1 = lineComparison.comparison ();
+        System.out.println ( "\nEnter line 2 coordinates" );
+        int line2 = lineComparison.comparison ();
+        lineComparison.equal ( line1,line2 );
+        lineComparison.compare ( line1,line2 );
+    }
+}
